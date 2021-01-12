c4b0c55308ee86ef01fa2e850a70b9d57b6fc606
Caleb Curry <calebcurry.com>
2021-01-12 11:41:32 -0500

    forgot a file lol

--- application.py
+++ application.py
@@ -1,14 +1,1 @@
-from flask import Flask
-
-app = Flask(__name__)
-application = app
-
-
-@app.route('/')
-def index():
-    return 'hello'
-
-
-@app.route('/api/testimonials')
-def testimonials():
-    return {'testimonial': ['great', 'its ok', ]}
+from testimonials import app as application
